cf0f395de5795b5d657f4d153178751864b9dd2a
LekmaDev <106377626+LekmaDev@users.noreply.github.com>
2022-11-12 02:04:49 +0600

    Delete Code (viamane plachet)

--- Classes/Packets/Server/Championship/SeasonRewards.py
+++ Classes/Packets/Server/Championship/SeasonRewards.py
@@ -12,22 +12,7 @@ class SeasonRewards(PiranhaMessage):
         
 
     def encode(self, fields, player):
-        self.writeVInt(4)#4 - championship, 5 - psg, 1 - table?? (??)
-        self.writeVInt(9)#count reward
-        for x in range(9):
-         #reward entry
-            self.writeVInt(x+1)#idx
-            self.writeVInt(1)#?
-            self.writeVInt(x)#win
-            self.writeBoolean(True)#gemoffer
-            self.writeVInt(6)#id
-            self.writeVInt(1)#count
-            self.writeVInt(16)#dref
-            self.writeVInt(0)
-            self.writeVInt(0)#skin
-            self.writeBoolean(False)#??
-            #reward entry end
-            self.writeBoolean(False)#??
+        pass
     	
 
     def decode(self):
